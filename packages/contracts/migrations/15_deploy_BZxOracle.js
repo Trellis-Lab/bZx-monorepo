412b98576d47b67b71d0cc2b13148c32cb36da90
sanchaymittal <sanchaymittal@gmail.com>
2019-09-08 22:00:30 +0530

    Oracle addresses;

--- packages/contracts/migrations/15_deploy_BZxOracle.js
+++ packages/contracts/migrations/15_deploy_BZxOracle.js
@@ -1,5 +1,3 @@
-
-
 const deployNotifier = false;
 
 var BZxOracle;
@@ -69,18 +67,18 @@ module.exports = (deployer, network, accounts) => {
       valueAmount = web3.utils.toWei("1", "ether");
     }
 
-    deployer.then(async function() {
+    deployer.then(async function () {
 
-      var oracleNotifier;
-      if (network == "development" || deployNotifier) {
-        oracleNotifier = await deployer.deploy(OracleNotifier);
-      } else {
-        if (!config["addresses"][network]["OracleNotifier"]) {
-          console.log("OracleNotifier address not found in config!");
-          process.exit();
-        }
-        oracleNotifier = await OracleNotifier.at(config["addresses"][network]["OracleNotifier"]);
-      }
+      // var oracleNotifier;
+      // if (network == "development" || deployNotifier) {
+      //   oracleNotifier = await deployer.deploy(OracleNotifier);
+      // } else {
+      //   if (!config["addresses"][network]["OracleNotifier"]) {
+      //     console.log("OracleNotifier address not found in config!");
+      //     process.exit();
+      //   }
+      //   oracleNotifier = await OracleNotifier.at(config["addresses"][network]["OracleNotifier"]);
+      // }
 
       await deployer.deploy(
         BZxOracle,
@@ -89,8 +87,10 @@ module.exports = (deployer, network, accounts) => {
         weth_token_address,
         bzrx_token_address,
         oracleNotifier.address,
-        "0x13ddac8d492e463073934e2a101e419481970299", // feeWallet
-        { from: accounts[0] }
+        "0x0000000000000000000000000000000000000000", // feeWallet
+        {
+          from: accounts[0]
+        }
       );
 
       const oracle = await BZxOracle.deployed();
@@ -100,31 +100,31 @@ module.exports = (deployer, network, accounts) => {
 
       if (network == "mainnet") {
         await oracle.setSupportedTokensBatch([
-          "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // ETH
-          "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
-          "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
-          "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359", // DAI
-          "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
-          "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2", // MKR
-          "0xdd974d5c2e2928dea5f71b9825b8b646686bd200", // KNC
-          "0x1985365e9f78359a9b6ad760e32412f4a445e862", // REP
-          "0x0d8775f648430679a709e98d2b0cb6250d2887ef", // BAT
-          "0xe41d2489571d322189246dafa5ebde1f4699f498", // ZRX
-          "0x514910771af9ca656af840dff83e8264ecf986ca"  // LINK
-        ],
-        [
-          "true", // ETH
-          "true", // WETH
-          "true", // USDC
-          "true", // DAI
-          "true", // WBTC
-          "true", // MKR
-          "true", // KNC
-          "true", // REP
-          "true", // BAT
-          "true", // ZRX
-          "true"  // LINK
-        ]
+            "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee", // ETH
+            "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
+            "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+            "0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359", // DAI
+            "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
+            "0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2", // MKR
+            "0xdd974d5c2e2928dea5f71b9825b8b646686bd200", // KNC
+            "0x1985365e9f78359a9b6ad760e32412f4a445e862", // REP
+            "0x0d8775f648430679a709e98d2b0cb6250d2887ef", // BAT
+            "0xe41d2489571d322189246dafa5ebde1f4699f498", // ZRX
+            "0x514910771af9ca656af840dff83e8264ecf986ca" // LINK
+          ],
+          [
+            "true", // ETH
+            "true", // WETH
+            "true", // USDC
+            "true", // DAI
+            "true", // WBTC
+            "true", // MKR
+            "true", // KNC
+            "true", // REP
+            "true", // BAT
+            "true", // ZRX
+            "true" // LINK
+          ]
         );
         // ETH,WETH,USDC,DAI,WBTC,MKR,KNC,REP,BAT,ZRX,LINK
 
@@ -138,17 +138,17 @@ module.exports = (deployer, network, accounts) => {
           "0x1985365e9f78359a9b6ad760e32412f4a445e862", // REP
           "0x0d8775f648430679a709e98d2b0cb6250d2887ef", // BAT
           "0xe41d2489571d322189246dafa5ebde1f4699f498", // ZRX
-          "0x514910771af9ca656af840dff83e8264ecf986ca"  // LINK
+          "0x514910771af9ca656af840dff83e8264ecf986ca" // LINK
         ]);
 
         await oracle.setMaxSourceAmountAllowedBatch([
-          "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
-          "0x514910771af9ca656af840dff83e8264ecf986ca"  // LINK
-        ],
-        [
-          "400000000",                // WBTC
-          "15000000000000000000000"   // LINK
-        ]
+            "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
+            "0x514910771af9ca656af840dff83e8264ecf986ca" // LINK
+          ],
+          [
+            "400000000", // WBTC
+            "15000000000000000000000" // LINK
+          ]
         );
 
         /*
@@ -168,11 +168,13 @@ module.exports = (deployer, network, accounts) => {
 
       var weth = await BZxEther.at(weth_token_address);
       if (!OLD_ORACLE_ADDRESS) {
-        await weth.deposit({ value: valueAmount });
+        await weth.deposit({
+          value: valueAmount
+        });
         await weth.transfer(oracleAddress, valueAmount);
       }
 
-      await oracleNotifier.transferBZxOwnership(oracleAddress);
+      // await oracleNotifier.transferBZxOwnership(oracleAddress);
 
       var bZxProxy = await BZxProxySettings.at(BZxProxy.address);
       await oracle.transferBZxOwnership(BZxProxy.address);
@@ -299,8 +301,8 @@ module.exports = (deployer, network, accounts) => {
         if (network == "development") {
           await oracle.slippageMultiplier(web3.utils.toWei("97", "ether")); // 3% slippage
 
-          for(let i=0; i <= 9; i++) {
-            let t = await artifacts.require("TestToken"+i);
+          for (let i = 0; i <= 9; i++) {
+            let t = await artifacts.require("TestToken" + i);
 
             let rate = (await bZxOracleOld.getTradeData(
               t.address,
